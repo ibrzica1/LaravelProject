b5ea5e21b6b5d625a831fe83939aa89235903a32
ibrzica1 <igorbrzica1@gmail.com>
2026-02-12 14:24:37 +0100

    Create UserWeatherSeeder

---
+++ database/seeders/UserWeatherSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Weather;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserWeatherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $city = $this->command->getOutput()->ask("Enter a city",);
+        if($city === null){
+            $this->command->getOutput()->error("You didnt enter a city");
+        }
+        $temperature = $this->command->getOutput()->ask("Enter the temperature",); 
+        if($temperature === null){
+            $this->command->getOutput()->error("You didnt enter a temperature");
+        }
+
+            Weather::create([
+                "city" => $city,
+                "temperature" => $temperature,
+            ]);
+        
+        $this->command->getOutput()->info("You entered a city $city with the temperature $temperature");
+    }
+}
