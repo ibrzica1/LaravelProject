4340952870f9ea9ab8e6d4a0ae240499ff77fd31
ibrzica1 <igorbrzica1@gmail.com>
2026-02-23 16:00:03 +0100

    Creating admin check middleware and rearaging routes

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,27 @@
 use App\Http\Controllers\ForecastController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\WeatherController;
+use App\Http\Middleware\AdminCheckMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','welcome')
 ->name("home");
-Route::middleware('auth')->prefix('admin')->group(function() {
+Route::middleware('auth')->group(function() {
     Route::get('/weather',[WeatherController::class, 'index'])
     ->name('weather.page');
-    Route::get('/add-weather',[WeatherController::class,'addWeatherPage'])
+    Route::get('/weather/{city:name}', [ForecastController::class, 'getWeatherSingle'])
+    ->name('weather.single');
+    Route::get('/forecasts', [ForecastController::class, 'forcastsPage'])
+    ->name('forecasts.page');
+    Route::view('forecasts/search','searchForecastPage')
+    ->name('forecasts.search.page');
+    Route::get('forecasts/search/results', [ForecastController::class, 'searchForecastCity'])
+    ->name('forecasts.search');
+});
+
+Route::middleware(AdminCheckMiddleware::class)->prefix('admin')
+->group(function() {
+     Route::get('/add-weather',[WeatherController::class,'addWeatherPage'])
     ->name('weather.add.page');
     Route::post('/add_weather',[WeatherController::class, 'addWeather'])
     ->name('weather.add');
@@ -20,18 +33,10 @@ Route::middleware('auth')->prefix('admin')->group(function() {
     ->name('edit.weather.page');
     Route::patch('/edit_weather/{weather}',[WeatherController::class, 'editWeather'])
     ->name('edit.weather');
-    Route::get('/weather/{city:name}', [ForecastController::class, 'getWeatherSingle'])
-    ->name('weather.single');
-    Route::get('/forecasts', [ForecastController::class, 'forcastsPage'])
-    ->name('forecasts.page');
     Route::get('/forecasts/add-page', [ForecastController::class, 'addForecastPage'])
     ->name('forecasts.add.page');
     Route::post('/forecasts/add', [ForecastController::class, 'addForecast'])
     ->name('forecasts.add');
-    Route::view('forecasts/search','searchForecastPage')
-    ->name('forecasts.search.page');
-    Route::get('forecasts/search/results', [ForecastController::class, 'searchForecastCity'])
-    ->name('forecasts.search');
 });
 
 Route::get('/dashboard', function () {
