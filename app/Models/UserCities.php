d064377b894442c4a95581420e609cb1e5685236
ibrzica1 <igorbrzica1@gmail.com>
2026-02-23 16:20:21 +0100

    Rename User Cities Model

---
+++ app/Models/UserCities.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserCities extends Model
+{
+    protected $table = 'user_cities';
+
+    protected $fillable = [
+        'city_id','user_id',
+    ];
+}
