43e898710638e64c8add2b8562a1594609a6d89c
ibrzica1 <igorbrzica1@gmail.com>
2026-02-24 17:14:28 +0100

    Create favorite function in UserCities Controller

--- app/Http/Controllers/UserCitiesController.php
+++ app/Http/Controllers/UserCitiesController.php
@@ -3,18 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserCities;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserCitiesController extends Controller
 {
-    public function favorite(Request $request)
+    public function favorite(Request $request, $city)
     {
         $user = Auth::user();
         if($user === null)
         {
             return redirect()->back()->with(['error'=>'You must be logged in']);
         }
-        echo "Test";
+        
+        UserCities::create([
+            'city_id' => $city,
+            'user_id' => $user->id
+        ]);
+
+        return redirect()->back();
     }
 }
